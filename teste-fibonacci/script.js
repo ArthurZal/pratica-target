5b8ba83f41f2ea66e7149a2418051f3b74034401
Arthur Zall <zallarthur@gmail.com>
2024-10-09 22:14:31 -0300

    Estruturando o loop que cálcula a sequência de acordo com o número informado

--- teste-fibonacci/script.js
+++ teste-fibonacci/script.js
@@ -3,5 +3,16 @@ function isFibonacci(number) {
     let a = 0;
     let b = 1;
 
-    
+    if (number === a || number === b) {
+        return alert(`O número ${number} pertence a sequência Fibonacci`);
+    }
+
+    while (b < number) {
+
+        let temporaryValue = b;
+        b = a + b;
+        a = temporaryValue;
+    }
+
+
 }
